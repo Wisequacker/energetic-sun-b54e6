1fb363d7468ee089b06826d901321bc9be99fcf3
Stackbit Code Editor <projects@stackbit.com>
2022-06-04 23:14:05 +0000

    index.tsx: changed

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -7,7 +7,7 @@ export default function ImageBlock(props) {
         return null;
     }
 
-    const getId = () => {
+    const getId: string = () => {
         return elementId as string;
     }
     const annotations = fieldPath
