4296ed5f8fc9a299bdf3027e36bb22f585cf7f05
Stackbit Code Editor <projects@stackbit.com>
2022-06-04 23:01:41 +0000

    index.tsx: changed

---
+++ src/components/molecules/heroImageBlock/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import classNames from 'classnames';
+
+export default function ImageBlock(props) {
+    const { elementId, className, url, altText = '', 'data-sb-field-path': fieldPath } = props;
+    if (!url) {
+        return null;
+    }
+    const annotations = fieldPath
+        ? { 'data-sb-field-path': [fieldPath, `${fieldPath}.url#@src`, `${fieldPath}.altText#@alt`, `${fieldPath}.elementId#@id`].join(' ').trim() }
+        : {};
+
+    return (
+        <img
+            id={elementId || null}
+            className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', className)}
+            src={url}
+            alt={altText}
+            style={{ height: "50%", width: "50%", marginLeft: "auto", marginRight: "auto" }}
+            {...annotations}
+        />
+    );
+}
