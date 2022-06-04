b6ce56b86d0fa401d779da6c6e6bc867b4e2c4e6
Stackbit Code Editor <projects@stackbit.com>
2022-06-04 23:20:05 +0000

    index.tsx: changed

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -12,7 +12,7 @@ export default function ImageBlock(props) {
     if (elementId) {
         id = elementId as string
     }
-    
+    console.log(typeof )
     let match = 'guitar' as string
     
 
