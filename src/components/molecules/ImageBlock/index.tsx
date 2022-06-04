33f6951fd9ef4e12ee8481302dba3e43702f0810
Stackbit Code Editor <projects@stackbit.com>
2022-06-04 23:13:54 +0000

    index.tsx: changed

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -15,7 +15,7 @@ export default function ImageBlock(props) {
         : {};
 
     return (
-        {getId === 'guitar' ? <img
+        {getId() === 'guitar' ? <img
             id={elementId || null}
             className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', className)}
             src={url}
